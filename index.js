044d4fde43818dc19534696584c3a44c4eb9193d
Nick Sette <njs2394@gmail.com>
2023-11-01 21:57:51 -0400

    reduce

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries = batteryBatches.reduce(function (accumulator, element) {
+    return element + accumulator
+}, 0)
+
+const MVPs = [
+    {
+        name: "Trout",
+        homers: 3
+    },
+    {
+        name: "Judge",
+        homers: 1
+    },
+    {
+        name: "Harper",
+        homers: 2
+    }
+]
+
+//Accumulator is the running count, element is what we pass 
+let totalMvps = MVPs.reduce(function (accumulator, element) {
+    return element.homers + accumulator
+}, 0)
+
+console.log(totalMvps)
